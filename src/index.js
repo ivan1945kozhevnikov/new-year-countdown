2e918f5225253339d951d21ecaf317ee8e032f0b
Ivan Kozhevnikov <kozhevnikovivan86@gmail.com>
2023-12-05 16:17:58 +0300

    Create update countdown

--- src/index.js
+++ src/index.js
@@ -5,8 +5,40 @@ const seconds = document.getElementById('seconds');
 
 const currentYear = new Date().getFullYear();
 //Возвращает текущий год
-console.log(currentYear);
-
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 //Возваращает дату Нового Года
-console.log(newYearTime);
+
+const updateCountdown = () => {
+  const currentTime = new Date();
+  //Текущая дата
+  const difference = newYearTime - currentTime;
+  //Разница дат в миллисекундах
+
+  const differenceDays = Math.floor(difference / 1000 / 60 / 60 / 24);
+  //Разница дней
+  const differenceHours = Math.floor(difference / 1000 / 60 / 60) % 24;
+  //Разница часов
+  const differenceMinutes = Math.floor(difference / 1000 / 60) % 60;
+  //Разница мигут
+  const differenceSeconds = Math.floor(difference / 1000) % 60;
+  //Разница секунд
+
+  days.innerHTML = differenceDays;
+  //Отрисовать разницу дней
+  hours.innerHTML =
+    differenceHours < 10 ? '0' + differenceHours : differenceHours;
+  //Отрисовать разницу часов
+  //Если меньше 10, добавить 0 в начало
+  minutes.innerHTML =
+    differenceMinutes < 10 ? '0' + differenceMinutes : differenceMinutes;
+  //Отрисовать разницу минут
+  //Если меньше 10, добавить 0 в начало
+  seconds.innerHTML =
+    differenceSeconds < 10 ? '0' + differenceSeconds : differenceSeconds;
+  //Отрисовать разницу секунд
+  //Если меньше 10, добавить 0 в начало
+};
+//Обновление обратного отсчета
+
+setInterval(updateCountdown, 1000);
+//Обновлять каждые 1000 миллисекунд
